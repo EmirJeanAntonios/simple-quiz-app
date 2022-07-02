22a8445b9fded71093c401bb8abb7e4c6037dade
Jean Antonios <emir.antonios@gmail.com>
2022-07-02 19:44:50 +0300

    added admin template

---
+++ backend/resources/views/layouts/admin.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Quiz App</title>
+
+    <link rel="stylesheet" href="{{ asset('vendors/feather/feather.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendors/ti-icons/css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendors/css/vendor.bundle.base.css') }}">
+
+
+    @yield('css')
+
+
+    <link rel="stylesheet" href="{{ asset('css/vertical-layout-light/style.css') }}">
+
+    <link rel="shortcut icon" href="images/favicon.png" />
+</head>
+
+<body class="sidebar-dark">
+    <div class="container-scroller">
+
+        @include('layouts.partials.header')
+
+        <div class="container-fluid page-body-wrapper">
+
+
+            @include('layouts.partials.sidebar')
+
+            <div class="main-panel">
+                <div class="content-wrapper">
+                    @yield('content')
+                </div>
+
+
+                @include('layouts.partials.footer')
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+    <script src="{{ asset('vendors/js/vendor.bundle.base.js') }}"></script>
+
+
+    <script src="{{ asset('vendors/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ asset('vendors/datatables.net/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
+    <script src="{{ asset('js/dataTables.select.min.js') }}"></script>
+
+
+
+    <script src="{{ asset('js/off-canvas.js') }}"></script>
+    <script src="{{ asset('js/hoverable-collapse.js') }}"></script>
+    <script src="{{ asset('js/template.js') }}"></script>
+    <script src="{{ asset('js/settings.js') }}"></script>
+    <script src="{{ asset('js/todolist.js') }}"></script>
+
+
+    <script src="{{ asset('js/dashboard.js') }}"></script>
+    <script src="{{ asset('js/Chart.roundedBarCharts.js') }}"></script>
+
+</body>
+
+</html>
---
+++ backend/resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row navbar-dark">
+    <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+ 
+    </div>
+    <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+        <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+            <span class="icon-menu"></span>
+        </button>
+
+        <ul class="navbar-nav navbar-nav-right">
+
+            <li class="nav-item nav-profile dropdown">
+                <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
+                    <img src="images/faces/face28.jpg" alt="profile" />
+                </a>
+                <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
+                    <a class="dropdown-item">
+                        <i class="ti-settings text-primary"></i>
+                        Settings
+                    </a>
+                    <a class="dropdown-item">
+                        <i class="ti-power-off text-primary"></i>
+                        Logout
+                    </a>
+                </div>
+            </li>
+        </ul>
+        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button"
+            data-toggle="offcanvas">
+            <span class="icon-menu"></span>
+        </button>
+    </div>
+</nav>
